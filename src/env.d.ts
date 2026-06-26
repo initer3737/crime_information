3b8dd62deaf63558e05b566759e5ff857c320041
puthut wahyu aji <puthutwahyuaji13@gmail.com>
2026-06-26 10:46:18 +0700

    first commit

--- src/env.d.ts
+++ src/env.d.ts
@@ -3,3 +3,12 @@ declare module '*.vue' {
   const component: DefineComponent<{}, {}, any>
   export default component
 }
+
+interface ImportMetaEnv {
+  readonly VITE_SUPABASE_URL: string
+  // tambahkan env lain di sini jika ada
+}
+
+interface ImportMeta {
+  readonly env: ImportMetaEnv
+}
